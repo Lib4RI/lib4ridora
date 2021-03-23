1fd8ea395235a8fed64c5ae6c8f2aa676bf6840f
Frank Hoesli <frank.hoesli@lib4ri.ch>
2021-03-24 00:58:35 +0100

    Internal documents: From fix for embargo-private switch

--- js/lib4ridora.internal_as_private.js
+++ js/lib4ridora.internal_as_private.js
@@ -5,17 +5,38 @@
  * 
  */
 
-<script type="text/javascript"><!--
-
+<script type='text/javascript'><!--
+	
 function PdfInternalAsPrivate(uuOrPdfId) {
 	var expId = ( uuOrPdfId != null && uuOrPdfId != '' ) ? ( 's-' + uuOrPdfId ) : '';
-	var docVer = document.getElementById("edit-file" + expId + "-document-version");
-	if ( docVer != null && docVer.value == 'internal document' ) {
-		var avail = document.getElementById("edit-file" + expId + "-availability");
-		for(c=0;c<avail.options.length;c++) {
-			if ( avail.options[c].value == "private" ) { avail.selectedIndex = c; break; }
+	var docVer = document.getElementById('edit-file' + expId + '-document-version');
+	var avail = document.getElementById('edit-file' + expId + '-availability');
+	if ( docVer != null && avail != null ) {
+		var eStyle = '';
+		if ( docVer.value == 'internal document' ) {
+			for(c=0;c<avail.options.length;c++) {
+				if ( avail.options[c].value == 'private' ) {
+					avail.selectedIndex = c;
+					eStyle = 'none';
+					break;
+				}
+			}
+		}
+		if ( eStyle == '' ) {
+			for(c=0;c<avail.options.length;c++) {
+				if ( avail.options[c].value == 'date' ) {
+					eStyle = 'block';
+					break;
+				}
+			}
+		}
+		if ( eStyle != '' ) {
+			var eTmp = document.getElementById('edit-file' + expId + '-embargo-date');
+			if ( eTmp != null ) {
+				eTmp.style.display = eStyle;
+			}
 		}
 	}
 }
-
+	
 //--></script>
